4cf86bfb065d1a4355725d88f707ec6ee1edce6b
bloodsoul <812566160@qq.com>
2020-12-29 10:34:18 +0800

    commit

--- app/src/main/java/com/blood/jetpackdemo/live/LiveFragment.kt
+++ app/src/main/java/com/blood/jetpackdemo/live/LiveFragment.kt
@@ -2,9 +2,12 @@ package com.blood.jetpackdemo.live
 
 import android.content.Context
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -21,6 +24,11 @@ import com.blood.jetpackdemo.R
  */
 class LiveFragment : Fragment() {
 
+    private var rootView: View? = null
+    private lateinit var textView: TextView
+
+    private val handler: Handler = Handler(Looper.getMainLooper())
+
     init {
         println("$this init")
     }
@@ -36,14 +44,19 @@ class LiveFragment : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.layout_fragment_live, container, false)
-        println("$this onCreateView $view")
-        return view
+        if (rootView == null) {
+            rootView = inflater.inflate(R.layout.layout_fragment_live, container, false)
+        }
+        println("$this onCreateView $rootView")
+        return rootView
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         println("$this onActivityCreated")
         super.onActivityCreated(savedInstanceState)
+
+        textView = rootView!!.findViewById(R.id.text)
+        println("$this onActivityCreated $textView")
 
         ViewModelProvider(this).get(LiveViewModel::class.java).data.observe(viewLifecycleOwner, Observer {
             println("$this data $it")
@@ -63,28 +76,32 @@ class LiveFragment : Fragment() {
     override fun onDetach() {
         super.onDetach()
         println("$this onDetach")
+
+        handler.postDelayed({
+            textView.text = "123456\n789456"
+        }, 2000)
     }
 
-/**
-I/System.out: LiveFragment{2a78f62} (33b45635-558b-43db-b90f-d30d7d47846e)} init
-I/System.out: LiveFragment{2a78f62} (33b45635-558b-43db-b90f-d30d7d47846e) id=0x7f0800a6 tag_live_fragment} onAttach
-I/System.out: LiveFragment{2a78f62} (33b45635-558b-43db-b90f-d30d7d47846e) id=0x7f0800a6 tag_live_fragment} onCreate
-I/System.out: LiveFragment{2a78f62} (33b45635-558b-43db-b90f-d30d7d47846e) id=0x7f0800a6 tag_live_fragment} onCreateView
-I/System.out: LiveFragment{2a78f62} (33b45635-558b-43db-b90f-d30d7d47846e) id=0x7f0800a6 tag_live_fragment}. onActivityCreated
-I/System.out: com.blood.jetpackdemo.live.LiveViewModel@fe42ec8 init
-I/System.out: LiveFragment{2a78f62} (33b45635-558b-43db-b90f-d30d7d47846e) id=0x7f0800a6 tag_live_fragment} onActivityCreated
+    /**
+    I/System.out: LiveFragment{2a78f62} (33b45635-558b-43db-b90f-d30d7d47846e)} init
+    I/System.out: LiveFragment{2a78f62} (33b45635-558b-43db-b90f-d30d7d47846e) id=0x7f0800a6 tag_live_fragment} onAttach
+    I/System.out: LiveFragment{2a78f62} (33b45635-558b-43db-b90f-d30d7d47846e) id=0x7f0800a6 tag_live_fragment} onCreate
+    I/System.out: LiveFragment{2a78f62} (33b45635-558b-43db-b90f-d30d7d47846e) id=0x7f0800a6 tag_live_fragment} onCreateView
+    I/System.out: LiveFragment{2a78f62} (33b45635-558b-43db-b90f-d30d7d47846e) id=0x7f0800a6 tag_live_fragment}. onActivityCreated
+    I/System.out: com.blood.jetpackdemo.live.LiveViewModel@fe42ec8 init
+    I/System.out: LiveFragment{2a78f62} (33b45635-558b-43db-b90f-d30d7d47846e) id=0x7f0800a6 tag_live_fragment} onActivityCreated
 
-I/System.out: LiveFragment{2a78f62} (33b45635-558b-43db-b90f-d30d7d47846e) id=0x7f0800a6 tag_live_fragment} onDestroyView
-I/System.out: com.blood.jetpackdemo.live.LiveViewModel@fe42ec8 onCleared
-I/System.out: LiveFragment{2a78f62} (33b45635-558b-43db-b90f-d30d7d47846e) id=0x7f0800a6 tag_live_fragment} onDestroy
-I/System.out: LiveFragment{2a78f62} (33b45635-558b-43db-b90f-d30d7d47846e) id=0x7f0800a6 tag_live_fragment} onDetach
+    I/System.out: LiveFragment{2a78f62} (33b45635-558b-43db-b90f-d30d7d47846e) id=0x7f0800a6 tag_live_fragment} onDestroyView
+    I/System.out: com.blood.jetpackdemo.live.LiveViewModel@fe42ec8 onCleared
+    I/System.out: LiveFragment{2a78f62} (33b45635-558b-43db-b90f-d30d7d47846e) id=0x7f0800a6 tag_live_fragment} onDestroy
+    I/System.out: LiveFragment{2a78f62} (33b45635-558b-43db-b90f-d30d7d47846e) id=0x7f0800a6 tag_live_fragment} onDetach
 
-I/System.out: LiveFragment{2a78f62} (efe2a2b6-5a28-436c-8f4b-f86578a797fe) id=0x7f0800a6 tag_live_fragment} onAttach
-I/System.out: LiveFragment{2a78f62} (efe2a2b6-5a28-436c-8f4b-f86578a797fe) id=0x7f0800a6 tag_live_fragment} onCreate
-I/System.out: LiveFragment{2a78f62} (efe2a2b6-5a28-436c-8f4b-f86578a797fe) id=0x7f0800a6 tag_live_fragment} onCreateView
-I/System.out: LiveFragment{2a78f62} (efe2a2b6-5a28-436c-8f4b-f86578a797fe) id=0x7f0800a6 tag_live_fragment}. onActivityCreated
-I/System.out: com.blood.jetpackdemo.live.LiveViewModel@77bc99c init
-I/System.out: LiveFragment{2a78f62} (efe2a2b6-5a28-436c-8f4b-f86578a797fe) id=0x7f0800a6 tag_live_fragment} onActivityCreated
- */
+    I/System.out: LiveFragment{2a78f62} (efe2a2b6-5a28-436c-8f4b-f86578a797fe) id=0x7f0800a6 tag_live_fragment} onAttach
+    I/System.out: LiveFragment{2a78f62} (efe2a2b6-5a28-436c-8f4b-f86578a797fe) id=0x7f0800a6 tag_live_fragment} onCreate
+    I/System.out: LiveFragment{2a78f62} (efe2a2b6-5a28-436c-8f4b-f86578a797fe) id=0x7f0800a6 tag_live_fragment} onCreateView
+    I/System.out: LiveFragment{2a78f62} (efe2a2b6-5a28-436c-8f4b-f86578a797fe) id=0x7f0800a6 tag_live_fragment}. onActivityCreated
+    I/System.out: com.blood.jetpackdemo.live.LiveViewModel@77bc99c init
+    I/System.out: LiveFragment{2a78f62} (efe2a2b6-5a28-436c-8f4b-f86578a797fe) id=0x7f0800a6 tag_live_fragment} onActivityCreated
+     */
 
 }
